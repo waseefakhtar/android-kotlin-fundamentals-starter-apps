df21fbbf6e1c93e74c77d76d7ffd9b52c098949e
Waseef Akhtar <waseef@degoo.com>
2020-06-08 00:28:52 +0500

    Filter the results

--- MarsRealEstate-Final/app/src/main/java/com/example/android/marsrealestate/network/MarsProperty.kt
+++ MarsRealEstate-Final/app/src/main/java/com/example/android/marsrealestate/network/MarsProperty.kt
@@ -23,5 +23,7 @@ data class MarsProperty(
         val id: String,
         @Json(name = "img_src") val imgSrcUrl: String,
         val type: String,
-        val price: Double
-)
+        val price: Double) {
+    val isRental
+        get() = type == "rent"
+}
