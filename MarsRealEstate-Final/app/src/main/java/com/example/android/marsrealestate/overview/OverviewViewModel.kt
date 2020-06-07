c9307b03c7fd1de2ad667d653202a6149e19f76d
Waseef Akhtar <waseef@degoo.com>
2020-06-07 23:51:25 +0500

    Display a grid of images with a RecyclerView

--- MarsRealEstate-Final/app/src/main/java/com/example/android/marsrealestate/overview/OverviewViewModel.kt
+++ MarsRealEstate-Final/app/src/main/java/com/example/android/marsrealestate/overview/OverviewViewModel.kt
@@ -42,10 +42,10 @@ class OverviewViewModel : ViewModel() {
     val response: LiveData<String>
         get() = _response
 
-    private val _property = MutableLiveData<MarsProperty>()
+    private val _properties = MutableLiveData<List<MarsProperty>>()
 
-    val property: LiveData<MarsProperty>
-        get() = _property
+    val properties: LiveData<List<MarsProperty>>
+        get() = _properties
 
     private var viewModelJob = Job()
 
@@ -67,12 +67,10 @@ class OverviewViewModel : ViewModel() {
             var getPropertiesDeferred = MarsApi.retrofitService.getProperties()
             try {
                 var listResult = getPropertiesDeferred.await()
-                if (listResult.size > 0) {
-                    _property.value = listResult[0]
-                }
+                _response.value = "Success: ${listResult.size} Mars properties retrieved"
+                _properties.value = listResult
             } catch (e: Exception) {
                 _response.value = "Failure: ${e.message}"
-
             }
         }
     }
