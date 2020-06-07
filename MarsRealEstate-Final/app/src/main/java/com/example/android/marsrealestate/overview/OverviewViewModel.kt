1e3a1de10fc29e5020bcbe2ebdea8aab4d095232
Waseef Akhtar <waseef@degoo.com>
2020-06-07 23:24:58 +0500

    Display an internet image with Glide

--- MarsRealEstate-Final/app/src/main/java/com/example/android/marsrealestate/overview/OverviewViewModel.kt
+++ MarsRealEstate-Final/app/src/main/java/com/example/android/marsrealestate/overview/OverviewViewModel.kt
@@ -42,6 +42,11 @@ class OverviewViewModel : ViewModel() {
     val response: LiveData<String>
         get() = _response
 
+    private val _property = MutableLiveData<MarsProperty>()
+
+    val property: LiveData<MarsProperty>
+        get() = _property
+
     private var viewModelJob = Job()
 
     private val coroutineScope = CoroutineScope(
@@ -62,7 +67,9 @@ class OverviewViewModel : ViewModel() {
             var getPropertiesDeferred = MarsApi.retrofitService.getProperties()
             try {
                 var listResult = getPropertiesDeferred.await()
-                _response.value = "Success: ${listResult.size} Mars properties retrieved"
+                if (listResult.size > 0) {
+                    _property.value = listResult[0]
+                }
             } catch (e: Exception) {
                 _response.value = "Failure: ${e.message}"
 
